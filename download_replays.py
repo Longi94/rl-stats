ce448c49eb5ff5f2d53a13bb98a8946fff8738ff
Long Tran <lngtrn94@gmail.com>
2020-01-18 22:27:54 +0100

    logging and folder structure

--- download_replays.py
+++ download_replays.py
@@ -10,32 +10,38 @@ from database import Database, ReplayRecord
 
 HOST = 'https://calculated.gg'
 
+log = logging.getLogger(__name__)
+
 
 def carball_parse(hash: str, output_dir: str):
-    pts_file = os.path.join(output_dir, f'{hash}.pts')
-    gzip_file = os.path.join(output_dir, f'{hash}.gzip')
+    pts_file = os.path.join(output_dir, 'stats', f'{hash}.pts')
+    gzip_file = os.path.join(output_dir, 'df', f'{hash}.gzip')
 
     if os.path.exists(pts_file) and os.path.exists(gzip_file):
         return
 
-    manager = carball.analyze_replay_file(os.path.join(output_dir, f'{hash}.replay'))
+    manager = carball.analyze_replay_file(os.path.join(output_dir, 'replays', f'{hash}.replay'))
 
-    with open(os.path.join(output_dir, f'{hash}.pts'), 'wb') as f:
+    with open(pts_file, 'wb') as f:
         manager.write_proto_out_to_file(f)
 
-    with gzip.open(os.path.join(output_dir, f'{hash}.gzip'), 'wb') as f:
+    with gzip.open(gzip_file, 'wb') as f:
         manager.write_pandas_out_to_file(f)
 
 
 def process_replay(replay, output_dir: str, existing_hashes):
+    download_url = HOST + replay['download']
+    hash = replay['hash']
+
+    if hash in existing_hashes is not None:
+        return None
+
+    logging.basicConfig(handlers=[logging.FileHandler(os.path.join(output_dir, 'logs', f'{hash}.log'))],
+                        format='%(asctime)s %(levelname)s %(threadName)s %(message)s',
+                        level=logging.DEBUG)
+
     try:
-        download_url = HOST + replay['download']
-        hash = replay['hash']
-
-        if hash in existing_hashes is not None:
-            return None
-
-        file_path = os.path.join(output_dir, f'{hash}.replay')
+        file_path = os.path.join(output_dir, 'replays', f'{hash}.replay')
 
         if os.path.exists(file_path):
             log.info(f'{file_path} already exists')
@@ -69,19 +75,22 @@ if __name__ == '__main__':
     parser.add_argument('--api-key', '-a', type=str, required=True)
     parser.add_argument('--output-dir', '-o', type=str, required=True)
     parser.add_argument('--processes', '-p', type=int, default=1)
+    parser.add_argument('--log', '-l', type=str, required=True)
     args = parser.parse_args()
 
-    logging.basicConfig(handlers=[logging.StreamHandler()],
+    logging.basicConfig(handlers=[logging.StreamHandler(), logging.FileHandler(args.log)],
                         format='%(asctime)s %(levelname)s %(threadName)s %(message)s',
                         level=logging.DEBUG)
-
-    log = logging.getLogger('downloader')
 
     log.info('Starting...')
 
     db = Database(args.output_dir)
 
     os.makedirs(args.output_dir, exist_ok=True)
+    os.makedirs(os.path.join(args.output_dir, 'replays'), exist_ok=True)
+    os.makedirs(os.path.join(args.output_dir, 'logs'), exist_ok=True)
+    os.makedirs(os.path.join(args.output_dir, 'stats'), exist_ok=True)
+    os.makedirs(os.path.join(args.output_dir, 'df'), exist_ok=True)
 
     json_response = requests.get(f'{HOST}/api/v1/replays?key={args.api_key}&playlist=28').json()
     get_replays_from_response(json_response, args.output_dir, db, args.processes)
